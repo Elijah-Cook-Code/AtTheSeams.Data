23f370d639635528056080912aad1cbc6c6f5d11
Elijah-Cook-Code <elijahthomas123479@gmail.com>
2025-02-22 14:50:30 -0500

    second

--- obj/Debug/net8.0/AtTheSeams.Data.AssemblyInfo.cs
+++ obj/Debug/net8.0/AtTheSeams.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtTheSeams.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a18a464652eb80aeea3c55360fa17e7bc05fb664")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtTheSeams.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtTheSeams.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
